a48e507a4b58fdb928b0e595dc3c78cc5a60db32
krugzkv <67159848+krugzkv@users.noreply.github.com>
2020-10-28 00:01:34 +0500

    после урока

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@ const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
-close.addEventListener("click", toggleModalAuth);
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
 
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
 const loginForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
+const passwordInput = document.querySelector('#password');
 const userName = document.querySelector('.user-name');
 const buttonOut =document.querySelector('.button-out');
 
@@ -16,10 +22,19 @@ let login = localStorage.getItem('login');
 
 
 function toggleModalAuth() {
-   modalAuth.classList.toggle('is-open');  
+   modalAuth.classList.toggle('is-open');
+   loginInput.style.borderColor = ''; 
+   passwordInput.style.borderColor = ''; 
+   if (modalAuth.classList.contains('is-open')){
+     disabledScroll();
+     }
+     else {
+      enabledScroll();
+     }
 }
 
 function authorized() {
+  
   function logOut() {
     login = null;
     localStorage.removeItem('gloDelivery');
@@ -37,21 +52,37 @@ function authorized() {
     console.log('login'); 
 }
 function notAuthorized() {
-    function logIn(event) {
+  function logIn(event) {
     event.preventDefault();
+    if (loginInput.value.trim(), passwordInput.value.trim()) {
+    
     login = loginInput.value;
+    password = passwordInput.value;
     localStorage.setItem('gloDelivery', login);
     toggleModalAuth();
+   
     buttonAuth.removeEventListener('click', toggleModalAuth);
     closeAuth.removeEventListener('click', toggleModalAuth);
     loginForm.removeEventListener('submit', logIn);
     loginForm.reset();
     checkAuth();
+    }
+    else{
+      loginInput.style.borderColor = 'red';
+      loginInput.value = '';
+      passwordInput.style.borderColor = 'red';
+      passwordInput.value = '';
+      swal('Проверте логин и пароль', '', 'warning');
+    }
   }  
     buttonAuth.addEventListener('click', toggleModalAuth);
     closeAuth.addEventListener('click', toggleModalAuth);
     loginForm.addEventListener('submit', logIn);
-    console.log('not auth');  
+    modalAuth.addEventListener('click', function (event) {
+      if (event.target.classList.contains('is-open')) {
+        toggleModalAuth();
+      }
+    })
 }
 function checkAuth() {
   if (login){
@@ -59,7 +90,6 @@ function checkAuth() {
     } 
     else {
       notAuthorized();
-      swal("Введите логин", "", "warning");
     }
 } 
 checkAuth();
