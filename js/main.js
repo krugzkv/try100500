d4db88ab5640192089cf9305aad56fbd72f03fc9
krugzkv <67159848+krugzkv@users.noreply.github.com>
2020-10-29 04:18:15 +0500

    finaly finished lesson

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const swiperContainer = document.querySelector('.swiper-container');
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
@@ -10,7 +11,7 @@ const loginForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const passwordInput = document.querySelector('#password');
 const userName = document.querySelector('.user-name');
-const buttonOut =document.querySelector('.button-out');
+const buttonOut = document.querySelector('.button-out');
 const cardsRestaurants = document.querySelector('.cards-restaurants');
 const containerPromo = document.querySelector('.container-promo');
 const restaurants = document.querySelector('.restaurants');
@@ -19,24 +20,35 @@ const logo = document.querySelector('.logo');
 const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
-let password ;
+let password;
+
+function validName(str) {
+  const regName = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/
+  return regName.test(str);
+}
+
+function validPassword(str) {
+  const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/
+  return regPassword.test(str);
+}
 
 function toggleModal() {
   modal.classList.toggle('is-open');
 }
 function toggleModalAuth() {
-   modalAuth.classList.toggle('is-open');
-   loginInput.style.borderColor = ''; 
-   passwordInput.style.borderColor = ''; 
-   if (modalAuth.classList.contains('is-open')){
-     disabledScroll();
-     }
-     else {
-      enabledScroll();
-     }
+  modalAuth.classList.toggle('is-open');
+  loginForm.reset();
+  document.getElementById('login').placeholder = '';
+  document.getElementById('password').placeholder = '';
+  if (modalAuth.classList.contains('is-open')) {
+    disabledScroll();
+  }
+  else {
+    enabledScroll();
+  }
 }
 function authorized() {
-  
+
   function logOut() {
     login = null;
     localStorage.removeItem('gloDelivery');
@@ -46,55 +58,55 @@ function authorized() {
     buttonOut.removeEventListener('click', logOut);
     checkAuth();
     userName.classList.remove('loged');
-    containerPromo.classList.remove('hide');
+    swiperContainer.classList.remove('hide');
     restaurants.classList.remove('hide');
     menu.classList.add('hide');
   }
-    userName.classList.toggle('loged');
-    userName.textContent = login;
-    buttonAuth.style.display = 'none';
-    userName.style.display = 'inline';
-    buttonOut.style.display = 'block';
-    buttonOut.addEventListener('click', logOut);
-  }
+  userName.classList.toggle('loged');
+  userName.textContent = login;
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+  buttonOut.addEventListener('click', logOut);
+}
 function notAuthorized() {
   function logIn(event) {
     event.preventDefault();
-    if (loginInput.value.trim(), passwordInput.value.trim()) {
-    login = loginInput.value;
-    password = passwordInput.value;
-    localStorage.setItem('gloDelivery', login);
-    toggleModalAuth();
-    buttonAuth.removeEventListener('click', toggleModalAuth);
-    closeAuth.removeEventListener('click', toggleModalAuth);
-    loginForm.removeEventListener('submit', logIn);
-    loginForm.reset();
-    checkAuth();
-    
+    if (validName(loginInput.value), validPassword(passwordInput.value)) {
+      login = loginInput.value;
+      password = passwordInput.value;
+      localStorage.setItem('gloDelivery', login);
+      toggleModalAuth();
+      buttonAuth.removeEventListener('click', toggleModalAuth);
+      closeAuth.removeEventListener('click', toggleModalAuth);
+      loginForm.removeEventListener('submit', logIn);
+      loginForm.reset();
+      checkAuth();
+
     }
-    else{
-      loginInput.style.borderColor = 'red';
-      passwordInput.style.borderColor = 'red';
+    else {
+      document.getElementById('login').placeholder = 'Введите логин';
+      document.getElementById('password').placeholder = 'Введите пароль';
       swal('Проверте логин и пароль', '', 'warning');
     }
-  }  
-    buttonAuth.addEventListener('click', toggleModalAuth);
-    closeAuth.addEventListener('click', toggleModalAuth);
-    loginForm.addEventListener('submit', logIn);
-    modalAuth.addEventListener('click', function (event) {
-      if (event.target.classList.contains('is-open')) {
-        toggleModalAuth();
-      }
-    })
+  }
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  loginForm.addEventListener('submit', logIn);
+  modalAuth.addEventListener('click', function (event) {
+    if (event.target.classList.contains('is-open')) {
+      toggleModalAuth();
+    }
+  })
 }
 function checkAuth() {
-  if (login){
-      authorized();
-    } 
-    else {
-      notAuthorized();
-    }
-} 
+  if (login) {
+    authorized();
+  }
+  else {
+    notAuthorized();
+  }
+}
 function createCardRestaurant() {
   const card = `
     <a class="card card-restaurant">
@@ -137,23 +149,26 @@ function createCardGoods() {
           </div>
         </div>
   `);
-  cardsMenu.insertAdjacentElement('beforeend',card);
+  cardsMenu.insertAdjacentElement('beforeend', card);
 }
 function openGoods(event) {
   if (userName.classList.contains('loged')) {
     const target = event.target;
     const restaurant = target.closest('.card-restaurant');
-  
-    if (restaurant) {
-    cardsMenu.textContent = '';
-    containerPromo.classList.add('hide');
-    restaurants.classList.add('hide');
-    menu.classList.remove('hide');
 
-    createCardGoods();
-    createCardGoods();
-    createCardGoods();
-  }
+    if (restaurant) {
+      cardsMenu.textContent = '';
+      swiperContainer.classList.add('hide');
+      restaurants.classList.add('hide');
+      menu.classList.remove('hide');
+
+      createCardGoods();
+      createCardGoods();
+      createCardGoods();
+      createCardGoods();
+      createCardGoods();
+      createCardGoods();
+    }
   } else {
     toggleModalAuth();
   }
@@ -163,9 +178,9 @@ cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
 cardsRestaurants.addEventListener('click', openGoods);
 logo.addEventListener('click', function () {
-   containerPromo.classList.remove('hide');
-   restaurants.classList.remove('hide');
-   menu.classList.add('hide');
+  swiperContainer.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
 })
 
 checkAuth();
@@ -173,4 +188,25 @@ checkAuth();
 createCardRestaurant();
 createCardRestaurant();
 createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
 
+new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    dragSize: 'auto',
+  },
+});
